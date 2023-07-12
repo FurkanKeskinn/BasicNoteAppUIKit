afb674abaae0f52b5aded191c0276522d79be954
Furkan Keskin <furkankeskin2603@gmail.com>
2023-07-12 13:52:50 +0300

    Added and included SwiftGen, SwiftLint.
    Add / resources files and Swiftgen stencil files added.
    Included / SwiftGen, SwiftLint included in the project.
    Remove / main stroyboard removed.

---
+++ BasicNoteApp/BasicNoteApp/Resources/Fonts/UIFont+Extensions.swift
@@ -0,0 +1,70 @@
+//
+//  UIFont+Extensions.swift
+//  BasicNoteApp
+//
+//  Created by Furkan on 12.07.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+
+  enum FontWeight {
+      case interRegular
+      case interLight
+      case interMedium
+      case interSemiBold
+      case interBold
+  }
+
+  // swiftlint:disable identifier_name
+  enum FontSize {
+      /// 13
+      case small
+
+      /// 40
+      case large
+
+      /// 27
+      case h1
+
+      /// 21
+      case h2
+
+      /// 17
+      case h3
+
+      /// custom
+      case custom(size: CGFloat)
+
+      public var rawValue: CGFloat {
+          switch self {
+          case .small: return 13
+          case .large:  return 40
+          case .h1: return 27
+          case .h2: return 21
+          case .h3: return 17
+          case .custom(let size):return size
+          }
+      }
+  }
+  // swiftlint:enable identifier_name
+
+  static func font(_ weight: UIFont.FontWeight, size: FontSize) -> UIFont {
+      let font: UIFont
+      switch weight {
+      case .interRegular:
+          font = FontFamily.Inter.regular.font(size: size.rawValue)
+      case .interLight:
+          font = FontFamily.Inter.light.font(size: size.rawValue)
+      case .interMedium:
+          font = FontFamily.Inter.medium.font(size: size.rawValue)
+      case .interSemiBold:
+          font = FontFamily.Inter.semiBold.font(size: size.rawValue)
+      case .interBold:
+          font = FontFamily.Inter.bold.font(size: size.rawValue)
+      }
+      return font
+  }
+}
